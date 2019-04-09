d92c1f3812548c826313eb8e836a185ddfdac0d4
Austin Hunter <austinhunter@u.boisestate.edu>
2019-04-09 14:58:11 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function add(a, b){
+    document.getElementById('addResult').innerHTML = (parseInt(a) + parseInt(b));
+}
+
+function multiply(a, b){
+    document.getElementById('multResult').innerHTML = (parseInt(a) * parseInt(b));
+}
+
+function divide(a, b){
+    document.getElementById('divResult').innerHTML = (parseInt(a)/parseInt(b));
+}
