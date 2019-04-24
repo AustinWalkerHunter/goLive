397c9ceb33155681ed9da84383778b71b427ac51
ZachKG <33558168+ZachKG@users.noreply.github.com>
2019-04-24 11:54:23 -0600

    Add files via upload

--- Quiz.js
+++ Quiz.js
@@ -53,3 +53,35 @@ function question2() {
     q2.style.display = "none";
     q3.style.display = "block";
 }
+
+function checkQ3(){
+    var x = document.getElementById("check3").value;
+    if(x == 25){
+        score += 15;
+        document.getElementById('modal3').style.backgroundColor = 'rgba(0, 171, 0, 0.42)';
+        document.getElementById('q3Answer').innerHTML = "Correct! +15 Points";
+        document.getElementById('q3Response').innerHTML = "Nice!";
+        document.getElementById('score').innerHTML = score;
+    }
+    else{
+        document.getElementById('modal3').style.backgroundColor = 'rgba(255, 0, 2, 0.75)';
+        document.getElementById('q3Answer').innerHTML = "Ouch...";
+        document.getElementById('q3Response').innerHTML = "Better luck next time!";
+    }
+}
+function question3() {
+    q3.style.display = "none";
+    q4.style.display = "block";
+}
+
+// function q3sum(){
+//     var intArray = new Array(5);
+//     intArray = [2, 3, 7, 12, 1];
+
+//     var i;
+//     var s=0;
+//     for(i = 0; i < 5; i++){
+//         s+=intArray[i];
+//     }
+//     return s;
+// }
